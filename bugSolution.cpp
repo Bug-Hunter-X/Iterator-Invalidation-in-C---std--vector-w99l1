5ad2851724137ad2d4f11ada2923b7f672dec615
BugHunter-X <askzenai@gmail.com>
2025-02-14 23:42:05 +0100

    Add bugSolution.cpp file

---
+++ bugSolution.cpp
@@ -0,0 +1,18 @@
+std::vector<int> vec; 
+for (int i = 0; i < 10; ++i) {
+  vec.push_back(i); 
+}
+
+// Correct approach 1: Using iterators
+for (auto it = vec.begin(); it != vec.end(); ) {
+  if (*it == 5) {
+    it = vec.erase(it);
+  } else {
+    ++it;
+  }
+}
+
+//Correct approach 2: Using std::remove and erase
+vec.erase(std::remove(vec.begin(), vec.end(), 5), vec.end());
+
+//Now the vector vec will have element 5 removed correctly.
